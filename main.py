36eb7db3bf8c3d1a4f18092ed5b800ff18dbf51f
Wilson Chen <wilsonc24@nycstudents.net>
2022-06-07 22:29:59 -0400

    added title screen

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ programIcon = pygame.image.load('./assets/orb.png')
 pygame.display.set_icon(programIcon)
 WORD_FONT = pygame.font.SysFont('garamond', 30)
 WIN_FONT = pygame.font.SysFont('garamond', 70)
+TITLE_FONT = pygame.font.SysFont('garamond', 90)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
@@ -26,9 +27,9 @@ SIZE = 75
 
 
 SUSPENSE = pygame.mixer.Sound('./assets/suspense.ogg')
+MYSTERY = pygame.mixer.Sound('./assets/mystery.mp3')
 BEEP = pygame.mixer.Sound('./assets/beep.wav')
 TYPE = pygame.mixer.Sound('./assets/type.wav')
-
 SEA = pygame.image.load('./assets/sea.jpg')
 SEA = pygame.transform.scale(SEA, (WIDTH, HEIGHT))
 TEXTBOX = pygame.image.load('./assets/textbox.png')
@@ -39,11 +40,21 @@ ORB = pygame.image.load('./assets/orb.png')
 ORB = pygame.transform.scale(ORB, (100, 100))
 OIL = pygame.image.load('./assets/oil.png')
 OIL = pygame.transform.scale(OIL, (OIL.get_width()/10, OIL.get_height()/10))
-
+OCEAN = pygame.image.load('./assets/ocean.jpg')
 BOX = pygame.USEREVENT + 1
 OIL_EVENT = pygame.USEREVENT +2
 pygame.time.set_timer(BOX, 500)
 print(type(OIL_EVENT))
+
+INTRO_SONG = MYSTERY
+GAME_SONG = SUSPENSE
+
+BUTTON = pygame.image.load('./assets/button.png')
+BUTTON = pygame.transform.scale(BUTTON, (400, 50))
+LARGER_BUTTON = pygame.transform.scale(BUTTON, (420, 50))
+TITLE = pygame.image.load('./assets/title.png')
+
+SPARK = pygame.image.load('./assets/spark.png')
 
 global text
 global line_numi
@@ -52,13 +63,13 @@ text_list = []
 
 def print_text():
     global text_len
-    TYPE.play()
+    #TYPE.play()
     space = 580
     for text in text_list:
         text_len += 1
-        WIN.blit(WORD_FONT.render(text[:text_len], True, WHITE), (135, space))
+        WIN.blit(WORD_FONT.render(text[:text_len], True, BLUE), (135, space))
         space += 30
-    TYPE.stop()
+    #TYPE.stop()
 
 def update_text():
     global line_num
@@ -93,12 +104,79 @@ text_len = 0
 #pygame.time.set_timer(TYPEWRITER, 100)
 
 
+def print_intro_window():
+
+    WIN.blit(OCEAN, (0,0))
+    WIN.blit(BUTTON,  (WIDTH/2 - BUTTON.get_width()/2, 250))
+    WIN.blit(BUTTON,  (WIDTH/2 - BUTTON.get_width()/2, 320))
+    WIN.blit(pygame.transform.scale(BUTTON, (190, 50)),  (WIDTH/2 - BUTTON.get_width()/2, 450))
+    thing = pygame.transform.flip(pygame.transform.scale(BUTTON, (190, 50)), True, False)
+    WIN.blit(thing,  (510, 450))
+    WIN.blit(TITLE, (WIDTH/2 - TITLE.get_width()/2, 70))
+    subtext = WORD_FONT.render("Singleplayer", True, BLUE)
+    WIN.blit(subtext, (WIDTH/2 - subtext.get_width()/2, 260))
+    WIN.blit(WORD_FONT.render("Multiplayer", True, BLUE), (WIDTH/2 - subtext.get_width()/2, 330))
+    WIN.blit(WORD_FONT.render("Quit", True, BLUE), (575, 460))
+    WIN.blit(WORD_FONT.render("Options", True, BLUE), (350, 460))
+
+def intro():
+    active = True
+    INTRO_SONG.set_volume(0.7)
+    INTRO_SONG.play(-1)
+    subtext = WORD_FONT.render("Singleplayer", True, BLUE)
+    subtext2 = WORD_FONT.render("Exit", True, BLUE)
+    while active:
+        pos = pygame.mouse.get_pos()
+        WIN.blit(OCEAN, (0,0))
+        WIN.blit(TITLE, (WIDTH/2 - TITLE.get_width()/2, 70))
+        #WIN.blit(SPARK, (0,0))
+
+
+        WIN.blit(BUTTON,  (WIDTH/2 - BUTTON.get_width()/2, 250)) #singleplayer
+        WIN.blit(BUTTON,  (WIDTH/2 - BUTTON.get_width()/2, 320)) #quit
+        smaller_button = pygame.transform.scale(BUTTON, (190, 50))
+        WIN.blit(smaller_button,  (WIDTH/2 - BUTTON.get_width()/2, 450)) #left small button
+        thing = pygame.transform.flip(pygame.transform.scale(BUTTON, (190, 50)), True, False)
+        WIN.blit(thing,  (510, 450)) #right small button
+        WIN.blit(subtext, (WIDTH/2 - subtext.get_width()/2, 260))
+
+        #WIN.blit(WORD_FONT.render("Multiplayer", True, BLUE), (WIDTH/2 - subtext.get_width()/2, 330))
+        WIN.blit(subtext2, (WIDTH/2 - subtext2.get_width()/2, 330))
+        WIN.blit(WORD_FONT.render("Mute", True, BLUE), (575, 460))
+        WIN.blit(WORD_FONT.render("Options", True, BLUE), (350, 460))
+
+        play_button = pygame.Rect(WIDTH/2 - BUTTON.get_width()/2, 250, 400, 50)
+        quit_button = pygame.Rect(WIDTH/2 - BUTTON.get_width()/2, 320, 400, 50)
+        #pygame.draw.rect(WIN, WHITE, 190, 50)
+        if play_button.collidepoint(pos):
+            WIN.blit(LARGER_BUTTON,  (WIDTH/2 - LARGER_BUTTON.get_width()/2, 250))
+            WIN.blit(subtext, (WIDTH/2 - subtext.get_width()/2, 260))
+        elif quit_button.collidepoint(pos):
+            WIN.blit(LARGER_BUTTON,  (WIDTH/2 - LARGER_BUTTON.get_width()/2, 320))
+            WIN.blit(subtext2, (WIDTH/2 - subtext2.get_width()/2, 330))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1:
+                    if play_button.collidepoint(pos):
+                        active = False
+                        INTRO_SONG.stop()
+                        return
+                    elif quit_button.collidepoint(pos):
+                        pygame.quit()
+                        sys.exit()
+        WIN.blit(SPARK, (0, 0))
+        pygame.display.update()
+
+
+
 
 def draw_window():
     global box
     global text
     WIN.blit(SEA, (0,0))
-
     #WIN.blit(ORB, (WIDTH/2 - ORB.get_width()/2, 200))
     #name = WORD_FONT.render("NOVA", True, BLUE)
     #WIN.blit(name, (WIDTH/2 - name.get_width()/2, 175))
@@ -121,18 +199,15 @@ def oil_dropping(drop):
 
 
 def main():
+    intro()
     global text
     global box
     box = TEXTBOX
     clock = pygame.time.Clock()
     run = True
-
     update_text()
-
-
-    SUSPENSE.set_volume(0.4)
-    SUSPENSE.play(-1)
-
+    GAME_SONG.set_volume(0.3)
+    GAME_SONG.play(-1)
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
@@ -161,13 +236,14 @@ def main():
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN:
-                if (event.key == pygame.K_RETURN or event.key == pygame.K_SPACE) or (event.type == pygame.MOUSEBUTTONUP):
-                    #pos = pygame.mouse.get_pos()
+                if (event.key == pygame.K_RETURN or event.key == pygame.K_SPACE):
                     update_text()
-
                 if event.key == pygame.K_b:
                     print(type(OIL_EVENT))
                     pygame.event.post(pygame.event.Event(OIL_EVENT))
+            elif (event.type == pygame.MOUSEBUTTONUP):
+                if event.button == 1:
+                    update_text()
         draw_window()
     pygame.quit()
     sys.exit()
